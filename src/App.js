9406b3db708c24df5d79170d12c95ca5e567253c
Nat Sharpe <sharpe1890@gmail.com>
2018-10-16 10:25:03 -0400

    Removes serviceworker, changes favicon to SalesLoft logo and inits App.js with header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        People of SalesLoft
+      </header>
+    </div>
+  );
+};
+
+export default App;
