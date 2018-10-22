385c390677917809d719380db3899298a91e41a5
Nat Sharpe <sharpe1890@gmail.com>
2018-10-21 21:44:26 -0400

    Adds additional comments

--- src/Components/FrequencyTable.js
+++ src/Components/FrequencyTable.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 
 const FrequencyTable = ({ people }) => {
 
+// Sorts an array of objects containing characters and count numbers. Returns a new array of the objects arranged in descending order accoridng to count.
   const sortDescending = newArray => {
-
     return newArray.sort(function(a,b){
       let comparison = -1;
       if (b['count'] > a['count']) {
@@ -14,19 +14,18 @@ const FrequencyTable = ({ people }) => {
     });
   };
 
+// Receives an object of character keys with object values. Takes the objects (character name and count) and pushes them into an array.
   const sortAllChars = chars => {
     let newArray = [];
-
     Object.keys(chars).forEach(key => {
       newArray.push(chars[key])
     });
-
     return sortDescending(newArray);
   };
 
+// Takes a list of people and finds the frequency of all unique characters in the local-part of each person's email address. Adds the characters to a table with the character as a key and the name and count as its values.
   const findUniqueChars = people => {
     const uniques = {};
-
     for (let ii = 0; ii < people.length; ii++) {
       let email = people[ii].email_address;
       for (let jj = 0; jj < email.length; jj++) {
@@ -41,7 +40,6 @@ const FrequencyTable = ({ people }) => {
         };
       };
     };
-
     return sortAllChars(uniques);
   };
   
--- src/Components/PeopleTable.js
+++ src/Components/PeopleTable.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+// Pulls 'people' data from state and uses it to populate a table with three rows.
 const PeopleTable = ({ people }) => {
-  console.log(people.length);
-
   const rows = people.map((row, index) => {
-    console.log(row)
     return (
       <tr key={index}>
         <td>{row.display_name}</td> 
