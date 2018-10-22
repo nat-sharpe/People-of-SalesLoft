1f0da60e25b6310f9f1a36036c85fc91f035424c
Nat Sharpe <sharpe1890@gmail.com>
2018-10-21 21:34:14 -0400

    Unifies styling across all pages

--- src/Components/LandingScreen.js
+++ src/Components/LandingScreen.js
@@ -10,15 +10,15 @@ class LandingScreen extends Component {
         <h1 className="landing-header">People of SalesLoft</h1>
         <p>An application for accessing and managing People data from SalesLoft's API. Built by Nat Sharpe using React.js.
         </p>
-        <h3><Link to="/people">People Records</Link></h3>
+        <h3><Link to="/people" style={{ textDecoration: 'none', color:'#1F85C0'}}>People Records</Link></h3>
         <p>
           Shows a list of People records that are available via the API.  Displays each Person’s name, email address, and job title.
         </p>
-        <h3><Link to="/frequency">Frequency Count</Link></h3>
+        <h3><Link to="/frequency" style={{ textDecoration: 'none', color:'#1F85C0'}}>Frequency Count</Link></h3>
         <p>
           Displays a frequency count of all the unique characters in all the email addresses, sorted by descending frequency count.
         </p>
-        <h3><Link to="/dupes">Possible Duplicates</Link></h3>
+        <h3><Link to="/dupes" style={{ textDecoration: 'none', color:'#1F85C0'}}>Possible Duplicates</Link></h3>
         <p>
           Shows a list of suggested possible duplicate People. 
         </p>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from 'react';
 import {Link} from 'react-router-dom';
+import '../index.css';
 
 const NavBar = () => {
   return (
     <nav className="main-header">
-      <header><Link to="/">People of SalesLoft</Link></header>
+      <header><Link to="/" style={{ textDecoration: 'none', color:'white' }}>People of SalesLoft</Link></header>
       <div>
-        <button><Link to="/people">People Records</Link></button> 
-        <button><Link to="/frequency">Frequency Count</Link></button>
-        <button><Link to="/dupes">Possible Duplicates</Link></button>
+        <button><Link to="/people" style={styles}>People Records</Link></button> 
+        <button><Link to="/frequency" style={styles}>Frequency Count</Link></button>
+        <button><Link to="/dupes" style={styles}>Possible Duplicates</Link></button>
       </div>
     </nav>
   )
 }
 
 export default NavBar;
+
+const styles = {
+  textDecoration: 'none', 
+  fontSize: 20, 
+  color:'#1F85C0'
+}
